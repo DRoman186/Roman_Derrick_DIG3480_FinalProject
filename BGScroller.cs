2df8b582419d0ded88473ac70102cd27fcee8d68
DRoman186 <46657144+DRoman186@users.noreply.github.com>
2019-04-23 22:00:44 -0400

    Add files via upload

---
+++ BGScroller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeZ;
+
+    private bool gameOver;
+	private Vector3 startPosition;
+
+	void Start ()
+	{
+        gameOver = false;
+		startPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.forward * newPosition;
+        if(gameOver)
+        {
+            scrollSpeed = 0;
+        }
+	}
+}
